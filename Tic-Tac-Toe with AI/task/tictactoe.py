95738fc651e8639918eb72b9be49492d1e231828
Val Bob <val.bob@example.com>
2021-03-07 15:58:21 +0200

    medium_level

--- Tic-Tac-Toe with AI/task/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe.py
@@ -69,8 +69,6 @@ def move_result(table, row, col):
         for cell in range(3):
             if table[row][cell] != last_mark:
                 break
-            else:
-                continue
         else:
             win = True
         if win:
@@ -78,8 +76,6 @@ def move_result(table, row, col):
         for cell in range(3):
             if table[cell][col] != last_mark:
                 break
-            else:
-                continue
         else:
             win = True
         if win:
@@ -88,16 +84,12 @@ def move_result(table, row, col):
             for cell in range(3):
                 if table[cell][cell] != last_mark:
                     break
-                else:
-                    continue
             else:
                 win = True
         if row + col == 2:
             for cell in range(3):
                 if table[2 - cell][cell] != last_mark:
                     break
-                else:
-                    continue
             else:
                 win = True
         break
@@ -113,11 +105,81 @@ def move_result(table, row, col):
 
 
 def easy_level(table):
-    print('Making move level "easy"')
     empty_cells = [(i, j) for i in range(3) for j in range(3) if table[i][j] == '_']
     random.seed()
     row, col = empty_cells[random.randint(0, len(empty_cells) - 1)]
     return row, col
+
+
+def medium_level(table):
+    threat_stop = None  # cell to prevent opponent win
+    cur_mark = 'O' if sum(row.count('X') for row in table) > sum(row.count('O') for row in table) else 'X'
+    opponent_mark = 'O' if cur_mark == 'X' else 'X'
+    for row in range(3):
+        cnt_me = 0
+        cnt_other = 0
+        for col in range(3):
+            cnt_me += 1 if table[row][col] == cur_mark else 0
+            cnt_other += 1 if table[row][col] == opponent_mark else 0
+            if table[row][col] == '_':
+                empty_cell = (row, col)
+        if cnt_me + cnt_other == 3:
+            continue
+        elif cnt_me == 2:
+            row, col = empty_cell
+            return row, col
+        elif threat_stop is None and cnt_other == 2:
+            threat_stop = empty_cell
+    for col in range(3):
+        cnt_me = 0
+        cnt_other = 0
+        for row in range(3):
+            cnt_me += 1 if table[row][col] == cur_mark else 0
+            cnt_other += 1 if table[row][col] == opponent_mark else 0
+            if table[row][col] == '_':
+                empty_cell = (row, col)
+        if cnt_me + cnt_other == 3:
+            continue
+        elif cnt_me == 2:
+            row, col = empty_cell
+            return row, col
+        elif threat_stop is None and cnt_other == 2:
+            threat_stop = empty_cell
+    cnt_me = 0
+    cnt_other = 0
+    for i in range(3):
+        cnt_me += 1 if table[i][i] == cur_mark else 0
+        cnt_other += 1 if table[i][i] == opponent_mark else 0
+        if table[i][i] == '_':
+            empty_cell = (i, i)
+        if cnt_me + cnt_other != 3:
+            if cnt_me == 2:
+                row, col = empty_cell
+                return row, col
+
+            elif threat_stop is None and cnt_other == 2:
+                threat_stop = empty_cell
+
+    cnt_me = 0
+    cnt_other = 0
+    for i in range(3):
+        cnt_me += 1 if table[2 - i][i] == cur_mark else 0
+        cnt_other += 1 if table[2 - i][i] == opponent_mark else 0
+        if table[2 - i][i] == '_':
+            empty_cell = (2 - i, i)
+        if cnt_me + cnt_other != 3:
+            if cnt_me == 2:
+                row, col = empty_cell
+                return row, col
+
+            elif threat_stop is None and cnt_other == 2:
+                threat_stop = empty_cell
+    if threat_stop:
+        row, col = threat_stop
+        return row, col
+
+    else:
+        return easy_level(table)
 
 
 def main_menu():
@@ -126,8 +188,8 @@ def main_menu():
         if command[0] == 'exit':
             return False
         elif command[0] == 'start' and len(command) == 3:
-            if (command[1] in {'user', 'easy'} and
-                    command[2] in {'user', 'easy'}):
+            if (command[1] in {'user', 'easy', 'medium'} and
+                    command[2] in {'user', 'easy', 'medium'}):
                 return command[1], command[2]
         print('Bad parameters!')
 
@@ -136,7 +198,11 @@ def player(table, input_source):
     if input_source == 'user':
         return human_input(table)
     elif input_source == 'easy':
+        print('Making move level "easy"')
         return easy_level(table)
+    elif input_source == 'medium':
+        print('Making move level "medium"')
+        return medium_level(table)
     else:
         print('There is no such player')
 
